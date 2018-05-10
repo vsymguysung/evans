4114957d1cde89c527729e373ecc1839300b1a36
ktr0731 <aizu.s1230022@gmail.com>
2018-05-10 18:53:04 +0900

    rebase directories

---
+++ pages/src/components/Navigator.vue
@@ -0,0 +1,76 @@
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <template v-if="this.$route.path != '/'">
+        <li>
+          <router-link to="/">
+            <span class="icon is-small">
+              <font-awesome-icon icon="angle-double-up" />
+            </span>
+            <span>Top</span>
+          </router-link>
+        </li>
+      </template>
+      <li>
+        <router-link to="/demo" :replace="this.$route.path != '/'">
+            <span class="icon is-small">
+              <font-awesome-icon icon="terminal" />
+            </span>
+          <span>Demo</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/install" :replace="this.$route.path != '/'">
+            <span class="icon is-small">
+              <font-awesome-icon icon="code" />
+            </span>
+          <span>Install</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/itunes" :replace="this.$route.path != '/'">
+            <span class="icon is-small">
+              <font-awesome-icon :icon="['fab', 'itunes-note']" />
+            </span>
+          <span>iTunes</span>
+        </router-link>
+      </li>
+      <li>
+        <a target="_blank" href="//github.com/ktr0731/evans">
+          <span class="icon is-small">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </span>
+          <span>GitHub</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import '@fortawesome/fontawesome/styles.css';
+import fontawesome from '@fortawesome/fontawesome';
+import { faEdit } from '@fortawesome/fontawesome-free-solid';
+import brands from '@fortawesome/fontawesome-free-brands';
+
+fontawesome.config = {
+  autoAddCss: false,
+};
+
+fontawesome.library.add(brands, faEdit);
+
+export default {
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  margin-top: 5vh;
+  margin-right: 1vw;
+}
+
+</style>
